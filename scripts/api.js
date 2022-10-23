dee03eb0f5342d5ac0476b33c75cf6960ad33abf
Quentin Almira <quentin.almira@gmail.com>
2022-10-23 16:46:44 +0200

    photographer gallery

---
+++ scripts/api.js
@@ -0,0 +1,44 @@
+/* charger l'id depuis l'URL */
+const lien = window.location.search;
+console.log(lien);
+const urlParams = new URLSearchParams(lien);
+console.log(urlParams);
+const id = urlParams.get("id");
+console.log(id);
+
+/*Recuperer les data photographes dans le json*/
+async function getPhotographers() {
+  let photographers = [];
+
+  await fetch("photographers.json")
+    .then((response) => response.json())
+    .then((data) => {
+      photographers = data.photographers;
+    })
+    .catch((error) => {
+      alert(
+        "Une erreur est survenue, veuillez contacter l'administrateur du site !! "
+      );
+      console.log(error);
+    });
+
+  return { photographers: [...photographers] };
+}
+
+/*Recuperer les data Media dans le json*/
+async function getMedia() {
+  let media = [];
+
+  await fetch("photographers.json")
+    .then((response) => response.json())
+    .then((data) => {
+      media = data.media;
+    })
+    .catch((error) => {
+      alert(
+        "Une erreur est survenue, veuillez contacter l'administrateur du site !! "
+      );
+      console.log(error);
+    });
+  return { media: [...media] };
+}
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,50 +1,103 @@
-/* charger l'id depuis l'URL */
-const lien = window.location.search;
-console.log(lien);
-const urlParams = new URLSearchParams(lien);
-console.log(urlParams);
-const id = urlParams.get("id");
-console.log(id);
-
-/*Recuperer les data photographes dans le json*/
-async function getPhotographers() {
-  let photographers = [];
-
-  await fetch("photographers.json")
-    .then((response) => response.json())
-    .then((data) => {
-      photographers = data.photographers;
-    })
-    .catch((error) => {
-      alert(
-        "Une erreur est survenue, veuillez contacter l'administrateur du site !! "
-      );
-      console.log(error);
-    });
-
-  return { photographers: [...photographers] };
-}
-
-/*----------------------------*/
-
-// Afficher les datas
+// -----------------------  // Afficher les datas
 async function init() {
   // Récupère les datas des photographes
   const { photographers } = await getPhotographers();
   let photographer = await filterPhotographer(photographers);
   displayData(photographer);
+  console.log(photographer);
 }
 init();
+
+// -----------------------  // Creation des éléments du Header
 
 const photographershead = document.querySelector("#content");
 
 async function displayData(photographer) {
-  console.log(photographer.name);
+  const pheader = document.createElement("section");
+
+  // photographer informations
+
+  photographershead.appendChild(pheader);
+  pheader.classList.add("photographer-page__header");
+
+  const pHeaderLeft = document.createElement("div");
+  pHeaderLeft.classList.add("photographer-page__header-left");
+  pheader.appendChild(pHeaderLeft);
+
+  const pHeaderLeftinfo = document.createElement("div");
+  pHeaderLeftinfo.classList.add("photographer-page__header-left__info");
+  pHeaderLeft.appendChild(pHeaderLeftinfo);
+
+  const cardName = document.createElement("h2");
+  cardName.classList.add("card__name");
+  pHeaderLeftinfo.appendChild(cardName);
+  cardName.textContent = `${photographer.name}`;
+
+  const cardLocation = document.createElement("p");
+  cardLocation.classList.add("card__location");
+  pHeaderLeftinfo.appendChild(cardLocation);
+  cardLocation.textContent = "London, UK";
+
+  const cardTagline = document.createElement("p");
+  cardTagline.classList.add("card__tagline");
+  pHeaderLeftinfo.appendChild(cardTagline);
+  cardTagline.textContent = "Voir le beau dans le quotidien";
+
+  // photographer contact button
+
+  const pContact = document.createElement("button");
+  pContact.classList.add("photographer-page__header-left__contact");
+  pheader.appendChild(pContact);
+  pContact.textContent = "Contactez-moi";
+
+  //   // photographer Id picture
+
+  const pID = document.createElement("div");
+  pID.classList.add("photographer-page__header-right");
+  pheader.appendChild(pID);
+
+  const idPicture = document.createElement("img");
+  idPicture.setAttribute(
+    "src",
+    `./assets/photographersID/${photographer.portrait}`
+  );
+  pID.appendChild(idPicture);
 }
+
+// -----------------------  // Filtrer par ID
 
 async function filterPhotographer(photographers) {
   let photographer = photographers.filter(
     (photographer) => photographer.id.toString() === id
   );
   return photographer[0];
+}
+
+// --------------------------------Medias--------------------------------
+
+async function lunch() {
+  const { media } = await getMedia();
+  displayMedia(media);
+}
+
+lunch();
+
+// -----------------------  // Creation des éléments de la gallery
+async function displayMedia(media) {
+  let photo = media.filter((photo) => photo.photographerId.toString() === id);
+  console.log(photo);
+
+  const photographersGallery = document.querySelector("#content");
+  const pGallery = document.createElement("section");
+  photographersGallery.appendChild(pGallery);
+  pGallery.classList.add("photographer-Gallery");
+
+  photo.forEach((img) => {
+    const pPicture = document.createElement("img");
+    pPicture.setAttribute(
+      "src",
+      `./assets/Portfolio/${img.photographerId}/${img.image}`
+    );
+    pGallery.appendChild(pPicture);
+  });
 }
